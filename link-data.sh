63f38376ff588988172735e83d1b118edc043ff7
srsnn <srsnng@hotmail.com>
2026-03-31 16:07:57 +0800

    chore

--- link-data.sh
+++ link-data.sh
@@ -1,3 +1,3 @@
 sudo mount -t cifs "//192.168.31.194/datasets/SUN RGB-D/sunrgbd" ~/tmp/windows_share -o credentials=/root/.smbcredentials,iocharset=utf8,dir_mode=0777,file_mode=0777
 
-mkdir ~/ws/py/mmdetection3d/data2/ && ln -s  ~/tmp/windows_share ~/ws/py/mmdetection3d/data2/sunrgbd
+mkdir -p ./data2/ && ln -s  ~/tmp/windows_share ~/ws/py/mmdetection3d/data2/sunrgbd
