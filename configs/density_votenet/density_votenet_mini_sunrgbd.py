7b7701b0ee805d24710fe90ffbee9d8e30214e02
srsnn <srsnng@hotmail.com>
2026-04-05 00:50:45 +0800

    chore format

--- configs/density_votenet/density_votenet_mini_sunrgbd.py
+++ configs/density_votenet/density_votenet_mini_sunrgbd.py
@@ -2,25 +2,26 @@
 # MiniSUNRGBD dataset (5-class desktop objects)
 
 _base_ = [
-    "../_base_/datasets/mini_sunrgbd_3d.py",
-    "../_base_/models/votenet.py",
-    "../_base_/schedules/schedule-3x.py",
-    "../_base_/default_runtime.py",
+    '../_base_/datasets/mini_sunrgbd_3d.py',
+    '../_base_/models/votenet.py',
+    '../_base_/schedules/schedule-3x.py',
+    '../_base_/default_runtime.py',
 ]
 
 # Model settings - 5 classes for MiniSUNRGBD
 model = dict(
     backbone=dict(
-        type="DensityAwarePointNet2",
+        type='DensityAwarePointNet2',
         in_channels=5,  # xyz + height (shift_height) + density
         num_points=(2048, 1024, 512, 256),
         radius=(0.2, 0.4, 0.8, 1.2),
         num_samples=(64, 32, 16, 16),
-        sa_channels=((64, 64, 128), (128, 128, 256), (128, 128, 256), (128, 128, 256)),
+        sa_channels=((64, 64, 128), (128, 128, 256), (128, 128, 256),
+                     (128, 128, 256)),
         fp_channels=((256, 256), (256, 256)),
     ),
     neck=dict(
-        type="CGNLLocalFusionNeck",
+        type='CGNLLocalFusionNeck',
         in_channels=256,  # FP output channels from DensityAwarePointNet2
         num_blocks=1,
         groups=4,
@@ -29,7 +30,7 @@ model = dict(
     bbox_head=dict(
         num_classes=5,
         bbox_coder=dict(
-            type="PartialBinBasedBBoxCoder",
+            type='PartialBinBasedBBoxCoder',
             num_sizes=5,
             num_dir_bins=12,
             with_rot=True,
@@ -46,27 +47,28 @@ model = dict(
 
 # Training dataloader
 train_dataloader = dict(
-    batch_size=20, dataset=dict(dataset=dict(data_root="data2/mini_sunrgbd/"))
-)
+    batch_size=20, dataset=dict(dataset=dict(data_root='data2/mini_sunrgbd/')))
 
 # Validation dataloader
-val_dataloader = dict(batch_size=20, dataset=dict(data_root="data2/mini_sunrgbd/"))
+val_dataloader = dict(
+    batch_size=20, dataset=dict(data_root='data2/mini_sunrgbd/'))
 
 # Test dataloader
-test_dataloader = dict(batch_size=20, dataset=dict(data_root="data2/mini_sunrgbd/"))
+test_dataloader = dict(
+    batch_size=20, dataset=dict(data_root='data2/mini_sunrgbd/'))
 
 # Auto scale LR for smaller dataset
 auto_scale_lr = dict(enable=True, base_batch_size=128)
 
 # Visualization
-vis_backends = [dict(type="LocalVisBackend"), dict(type="TensorboardVisBackend")]
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='TensorboardVisBackend')
+]
 visualizer = dict(
-    type="Det3DLocalVisualizer", vis_backends=vis_backends, name="visualizer"
-)
+    type='Det3DLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 
-default_hooks = dict(
-    checkpoint=dict(type="CheckpointHook", interval=3),
-)
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=3), )
 
 # Work directory
-work_dir = "work_dirs/density_votenet_mini_sunrgbd"
+work_dir = 'work_dirs/density_votenet_mini_sunrgbd'
--- configs/density_votenet/density_votenet_mini_sunrgbd_finetune.py
+++ configs/density_votenet/density_votenet_mini_sunrgbd_finetune.py
@@ -20,36 +20,22 @@ param_scheduler = [
     # Warmup阶段 (begin=0, 第一个epoch是warmup)
     dict(type='LinearLR', start_factor=0.001, by_epoch=False, end=1),
     # 主训练阶段 (20 epoch, begin=0, end=20表示0-19epoch)
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=20,
-        milestones=[15, 18],
-        gamma=0.1)
+    dict(type='MultiStepLR', begin=0, end=20, milestones=[15, 18], gamma=0.1),
 ]
 
 # 训练配置
 train_dataloader = dict(
-    batch_size=20,
-    dataset=dict(
-        dataset=dict(
-            data_root='data2/mini_sunrgbd/')))
+    batch_size=20, dataset=dict(dataset=dict(data_root='data2/mini_sunrgbd/')))
 
 # 验证配置
 val_dataloader = dict(
-    batch_size=20,
-    dataset=dict(
-        data_root='data2/mini_sunrgbd/'))
+    batch_size=20, dataset=dict(data_root='data2/mini_sunrgbd/'))
 
 # Test dataloader
 test_dataloader = dict(
-    batch_size=20,
-    dataset=dict(
-        data_root='data2/mini_sunrgbd/'))
+    batch_size=20, dataset=dict(data_root='data2/mini_sunrgbd/'))
 
-default_hooks = dict(
-    checkpoint=dict(type='CheckpointHook', interval=3),
-)
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=3), )
 
 # Work directory
 work_dir = 'work_dirs/density_votenet_mini_sunrgbd_finetune'
