d4e932c482d0a4ec221383b2b3412c357dea0612
srsnn <srsnng@hotmail.com>
2026-04-05 00:38:14 +0800

    feat 优化数据挂载脚本

--- link-data.sh
+++ link-data.sh
@@ -1,3 +1,12 @@
-sudo mount -t cifs "//192.168.31.194/datasets/SUN RGB-D/sunrgbd" ~/tmp/windows_share -o credentials=/root/.smbcredentials,iocharset=utf8,dir_mode=0777,file_mode=0777
+mkdir -p ./data2/
 
-mkdir -p ./data2/ && ln -s  ~/tmp/windows_share ~/ws/py/mmdetection3d/data2/sunrgbd
+# sun rgbd
+mkdir -p ~/tmp/windows_share/sunrgbd
+sudo mount -t cifs "//192.168.31.194/datasets/SUN RGB-D/sunrgbd" ~/tmp/windows_share/sunrgbd -o credentials=/root/.smbcredentials,iocharset=utf8,dir_mode=0777,file_mode=0777
+ln -s  ~/tmp/windows_share/sunrgbd ~/ws/py/mmdetection3d/data2/sunrgbd
+
+# TO Secne-down
+mkdir -p ~/tmp/windows_share/TO-scannet
+mkdir -p data2/TO-SCENE-down
+sudo mount -t cifs "//192.168.31.194/datasets/TO-Scene-down/TO-scannet" ~/tmp/windows_share/TO-scannet -o credentials=/root/.smbcredentials,iocharset=utf8,dir_mode=0777,file_mode=0777
+ln -s  ~/tmp/windows_share/TO-scannet data2/TO-SCENE-down/TO-scannet
